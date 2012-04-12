1fce2e7827bb708c4cf8ce3fb08703e1e87c9dfb
Patrick Lange <patrick@thrillist.com>
2012-04-12 11:29:44 -0400

    exported prompt logic

---
+++ .bashrc.d/prompt.bash
@@ -0,0 +1,5 @@
+#! /bin/bash
+if [ $(type -t __git_ps1) == 'function' ]; then
+  export GIT_PS1_SHOWDIRTYSTATE=1
+  export PS1="$(txt bldblk '[\t]')$(txt txtgrn '[\u')$(txt txtcyn ':')$(txt txtgrn '\W$(__git_ps1 "(%s)")]')\$ "
+fi
