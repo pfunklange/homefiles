c75cf0a04f14da99f6feee2eded221bc34b96cc3
Patrick Lange <patrick@thrillist.com>
2012-04-13 10:27:37 -0400

    added host to PS1, changed '$' to '\$'.

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 if [ $(type -t __git_ps1) == 'function' ]; then
   export GIT_PS1_SHOWDIRTYSTATE=1
-  export PS1="$(txt bldblk '[\t]')$(txt txtgrn '[\u')$(txt txtcyn ':')$(txt txtgrn '\W$(__git_ps1 "(%s)")]')\$ "
+  export PS1="$(txt bldblk '[\t]')$(txt txtgrn '[\u')$(txt txtcyn '@')$(txt txtgrn '\h')$(txt txtcyn ':')$(txt txtgrn '\W$(__git_ps1 "(%s)")]')\\$ "
 fi
