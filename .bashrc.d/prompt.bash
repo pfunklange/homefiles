cc70c06f2db62c3283e88bd50a8ecad006938ba1
Patrick Lange <patrick@thrillist.com>
2012-04-17 15:11:32 -0400

    use PROMPT_COMMAND to set window title in screen/xterm

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -3,3 +3,14 @@ if [ $(type -t __git_ps1) == 'function' ]; then
   export GIT_PS1_SHOWDIRTYSTATE=1
   export PS1="$(txt bldblk '[\t]')$(txt txtgrn '[\u')$(txt txtcyn '@')$(txt txtgrn '\h')$(txt txtcyn ':')$(txt txtgrn '\W$(__git_ps1 "(%s)")]')\\$ "
 fi
+
+case $TERM in
+xterm*|rxvt*)
+  PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}[`basename ${PWD}`]\007"'
+  ;;
+screen*)
+  PROMPT_COMMAND='echo -ne "\033k[`basename ${PWD}`]\033\\"'
+  ;;
+*)
+  ;;
+esac
